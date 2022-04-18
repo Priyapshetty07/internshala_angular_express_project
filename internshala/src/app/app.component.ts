f671906d9312f5c1c52944a0ba9248ffb09323d2
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-18 12:02:58 +0530

    remove the header

--- internshala/src/app/app.component.ts
+++ internshala/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'internshala';
+
+
+showHead: boolean = false;
+
+  constructor(private router: Router) {
+
+    router.events.forEach((event) => {
+
+      if (event instanceof NavigationStart) {
+
+        if (event['url'] == '/about-us') {
+
+          this.showHead = false;
+
+        } else {
+
+          this.showHead = true;
+
+        }
+
+      }
+
+    });
+
+  }
 }
