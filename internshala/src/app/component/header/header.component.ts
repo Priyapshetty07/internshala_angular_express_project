5376563ad24ceaaec1383b03ce46ad4302ce3550
Priyapshetty07 <priyapshetty007@gmail.com>
2022-04-29 00:06:21 +0530

    modifed footer and dropdown

--- internshala/src/app/component/header/header.component.ts
+++ internshala/src/app/component/header/header.component.ts
@@ -13,27 +13,27 @@ export class HeaderComponent implements OnInit {
   internship = [
 
     {
-      category: "location", options: [
+      category: "Location", options: [
 
         { title: "Work from Home", link: "https://www.google.com" },
 
-        { title: "Internship in Bangal", link: "https://www.google.com" },
+        { title: "Internship in Bangalore", link: "https://www.google.com" },
 
         { title: "Internship in Delhi", link: "https://www.google.com" },
 
-        { title: "Internship in Hydera", link: "https://www.google.com" },
+        { title: "Internship in Hyderabad", link: "https://www.google.com" },
 
         { title: "Internship in Mumbai", link: "https://www.google.com" },
 
-        { title: "Internship in Chenna", link: "https://www.google.com" },
+        { title: "Internship in Chennai", link: "https://www.google.com" },
 
         { title: "Internship in Pune", link: "https://www.google.com" },
 
-        { title: "Internship in Kolkat", link: "https://www.google.com" },
+        { title: "Internship in Kolkata", link: "https://www.google.com" },
 
         { title: "Internship in Jaipur", link: "https://www.google.com" },
 
-        { title: "International Intern", link: "https://www.google.com" },
+        { title: "International Internship", link: "https://www.google.com" },
 
         { title: "View all internships", link: "https://www.google.com" }
 
@@ -42,7 +42,7 @@ export class HeaderComponent implements OnInit {
     },
 
     {
-      category: "profile", options: [
+      category: "Profile", options: [
 
         { title: "Computer Science Internship", link: "https://www.google.com" },
 
@@ -62,7 +62,13 @@ export class HeaderComponent implements OnInit {
 
         { title: "Law Internship", link: "https://www.google.com" },
 
-        { title: "International Intern", link: "https://www.google.com" },
+        { title: "Electrinics Internship", link: "https://www.google.com" },
+
+        { title: "Content Writing Internship", link: "https://www.google.com" },
+
+        { title: "Civil Internship", link: "https://www.google.com" },
+
+        { title: "Campus Ambassador Program", link: "https://www.google.com" },
 
         { title: "View all internships", link: "https://www.google.com" }
 
@@ -70,19 +76,34 @@ export class HeaderComponent implements OnInit {
 
     },
 
-    { category: "category", options: [{ title: "Work from Home", link: "https://www.google.com" }] },
+    {
+      category: "Category", options: [
+        { title: "Engineering Intership", link: "https://www.google.com" },
+        { title: "MBA Internship", link: "https://www.google.com" },
+        { title: "Part-Time Jobs/Internship", link: "https://www.google.com" },
+        { title: "Humanities Intership", link: "https://www.google.com" },
+        { title: "Science Intership", link: "https://www.google.com" },
+        { title: "Interships with job Offer", link: "https://www.google.com" },
+        { title: "Internships for Women", link: "https://www.google.com" },
+        { title: "View all internships", link: "https://www.google.com" },
+
+      ]
+    },
 
     {
-      category: "specializations", options: [{ title: "Digital Marketing Specialization", link: "" },
+      category: "Specializations", options: [
+        { title: "Digital Marketing Specialization", link: "" },
 
-      { title: "Human Resource Management Specialization", link: "#" },
+        { title: "Human Resource Management Specialization", link: "#" },
 
-      { title: "Data Science Specialization", link: "#" }]
+        { title: "Data Science Specialization", link: "#" }]
     }
 
   ]
-  onlineTrainings=[
+  onlineTrainings = [
     "Web Development",
+    "Java",
+    "API",
     "Digital Marketing",
     "Machine Learning",
     "Advanced Excel",
@@ -93,36 +114,34 @@ export class HeaderComponent implements OnInit {
   jobs = [
 
     {
-      category: "location", options: [
+      category: "Location", options: [
 
         { title: "Work from Home", link: "https://www.google.com" },
 
-        { title: "Internship in Bangal", link: "https://www.google.com" },
+        { title: "Jobs in Delhi", link: "https://www.google.com" },
 
-        { title: "Internship in Delhi", link: "https://www.google.com" },
+        { title: "Jobs in Mumbai", link: "https://www.google.com" },
 
-        { title: "Internship in Hydera", link: "https://www.google.com" },
+        { title: "Jobs in Bangalore", link: "https://www.google.com" },
 
-        { title: "Internship in Mumbai", link: "https://www.google.com" },
+        { title: "Jobs in Hyderabad", link: "https://www.google.com" },
 
-        { title: "Internship in Chenna", link: "https://www.google.com" },
+        { title: "Jobs in Kolkata", link: "https://www.google.com" },
 
-        { title: "Internship in Pune", link: "https://www.google.com" },
+        { title: "Jobs in Chennai", link: "https://www.google.com" },
 
-        { title: "Internship in Kolkat", link: "https://www.google.com" },
+        { title: "Jobs in Pune", link: "https://www.google.com" },
 
-        { title: "Internship in Jaipur", link: "https://www.google.com" },
+        { title: "Jobs in Jaipur", link: "https://www.google.com" },
 
-        { title: "International Intern", link: "https://www.google.com" },
-
-        { title: "View all internships", link: "https://www.google.com" }
+        { title: "View all jobs", link: "https://www.google.com" }
 
       ]
 
     },
 
     {
-      category: "category", options: [
+      category: "Category", options: [
 
         { title: "Marketing Job", link: "https://www.google.com" },
 
@@ -132,7 +151,7 @@ export class HeaderComponent implements OnInit {
 
         { title: "Architecture Job", link: "https://www.google.com" },
 
-        { title: "Mechanical Job", link: "https://www.google.com" },
+        { title: "Fresher jobs", link: "https://www.google.com" },
 
         { title: "HR Job", link: "https://www.google.com" },
 
@@ -140,7 +159,7 @@ export class HeaderComponent implements OnInit {
 
         { title: "Law Job", link: "https://www.google.com" },
 
-        { title: "International Job", link: "https://www.google.com" },
+        { title: "Computer science jobs", link: "https://www.google.com" },
 
         { title: "View all Job", link: "https://www.google.com" }
 
