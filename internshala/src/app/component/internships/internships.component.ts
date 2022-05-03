72c8b12f96db9354710191e1508a3afe0eee04f5
Priyapshetty07 <priyapshetty007@gmail.com>
2022-05-03 08:11:22 +0530

    added card

--- internshala/src/app/component/internships/internships.component.ts
+++ internshala/src/app/component/internships/internships.component.ts
@@ -8,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class InternshipsComponent implements OnInit {
 
   constructor() { }
-
+internship=[{title:"infosys support intern",subtitle:"Graphic design",desc:"$500/month",location:"bangalore",link:"https://google.com"},
+{title:"google internship",subtitle:"Web Development intern",desc:"$400/month",location:"shimoga",link:"https://google.com"},
+{title:"jnnce internship",subtitle:"Java Developer intern",desc:"$800/month",location:"mumbai",link:"https://google.com"},
+{title:"thbs internship",subtitle:"Python Developer intern",desc:"$100/month",location:"chennai",link:"https://google.com"}]
   ngOnInit(): void {
   }
 
