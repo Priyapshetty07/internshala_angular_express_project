006a789147e9466a6ca8fb5010a1ccbf00b84707
Priyapshetty07 <priyapshetty007@gmail.com>
2022-05-03 13:03:41 +0530

    added post method-using postman

--- internshala/internshala-api/routes/users.js
+++ internshala/internshala-api/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 let dbConnection = require('./../db/data').localConnect();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -13,5 +14,43 @@ router.get('/login', function (req, res, next) {
   });
   // res.send('respond with a resource');
 });
+
+//post methods-login
+
+router.post('/login', (req, res, next) => {
+  let {
+   email,
+   paswd,
+   firstname,
+   lastname
+    
+  } = req.body
+
+  let insertcommand = `INSERT INTO internshala.login  (email,paswd,firstname,lastname)
+   VALUES ('${email}','${paswd}','${ firstname}','${lastname}')`;
+
+  dbConnection.query(insertcommand, (err, result) => {
+
+    if (err) throw err;
+
+    res.send("detailes are fetched from postman api detailes are inserted");
+
+  });
+
+})
+
+router.get('/login', function (req, res, next) {
+
+  dbConnection.query('SELECT * FROM internshala.login', (error, results, fields) => {
+    if (error) throw error;
+    res.send(results);
+
+  });
+
+  //res.send('respond with a resource');
+
+});
+
+
 
 module.exports = router;
